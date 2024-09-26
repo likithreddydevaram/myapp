f58b06621e15093ccb4758835e98c012abb8e12b
Likith Reddy <likithdevaram@gmail.com>
2024-09-26 07:02:15 +0530

    fourth comit

--- src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
+++ src/components/core/Dashboard/InstructorCourses/CoursesTable.jsx
@@ -110,7 +110,7 @@ export default function CoursesTable({ courses, setCourses }) {
                   </div>
                 </Td>
                 <Td className="text-sm font-medium text-richblack-100">
-                  5sec
+                  5seconds
                 </Td>
                 <Td className="text-sm font-medium text-richblack-100">
                   ₹{course.price}
