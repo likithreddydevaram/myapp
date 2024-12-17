660c194bbec71751e352dbea91619c00f5c464b3
Likith Reddy <likithdevaram@gmail.com>
2024-12-17 11:34:52 +0530

    timeout change

--- src/services/apiConnector.jsx
+++ src/services/apiConnector.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_HOST}/api/v1`, // Your base URL
-  timeout: 10000, // Optional: Set a timeout for requests
+  timeout: 30000, // Optional: Set a timeout for requests
   headers: {
     'Content-Type': 'application/json', // Default content type
   },
